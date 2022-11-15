68713a75e53fdca6c567cfeaf1b17d6c366eb87a
yongri910 <yongri910@gmail.com>
2022-11-15 20:13:17 +0800

    js: moreBtn show/hidden

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 const moreBtn = document.querySelector('.info .metadata .moreBtn');
 const title = document.querySelector('.info .metadata .title');
 
+// toggle moreBtn
 moreBtn.addEventListener('click', () => {
   moreBtn.classList.toggle('clicked');
   title.classList.toggle('clamp');
 });
+
+// show/hidden moreBtn
+window.addEventListener('resize', () => { showMoreBtn(title) });
+window.addEventListener('load', () => { showMoreBtn(title) });
+
+function showMoreBtn(element) {
+  if (element.offsetHeight < element.scrollHeight) {
+    moreBtn.style.visibility = "visible";
+  } else {
+    moreBtn.style.visibility = "hidden";    
+  }
+}
